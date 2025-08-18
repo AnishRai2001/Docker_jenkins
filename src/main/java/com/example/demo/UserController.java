bbeeacbe0cee0b601769afeb93d33ce75d877a1f
AnishRai2001 <anishrai2023ee@gmail.com>
2025-08-18 11:05:00 +0530

    Update UserController.java
    changes made

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 	
-	@GetMapping
+	@GetMapping("/message")
 	public String getMessage() {
 		return "hello from anish";
 	}
